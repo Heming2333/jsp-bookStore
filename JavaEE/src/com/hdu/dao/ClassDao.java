e01d59b7df40d2a0e03e369b8cfe5722f3f5f07b
YuXLan <1763630782@qq.com>
2017-10-08 21:32:03 +0800

    jsp bookstore

--- JavaEE/src/com/hdu/dao/ClassDao.java
+++
@@ -1,7 +0,0 @@
-package com.hdu.dao;
-import java.util.List;
-
-import com.hdu.model.Class;
-public interface ClassDao {
-	public List<Class> findAllClass();
-}
--- JavaEE/src/com/hdu/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.hdu.dao;
-
-import com.hdu.model.User;
-
-public interface UserDao {
-	public void add(User user);
-	public User selectByUsername(int username);
-}
